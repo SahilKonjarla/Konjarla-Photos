f0b7f74013e5c3f04b638288c215fd156fe35440
Sahil Konjarla <sahil.konjarla@gmail.com>
2022-08-08 19:49:28 -0700

    starting dynamic calls

--- js/pics.js
+++ js/pics.js
@@ -1,8 +1,11 @@
-//var searchhost = "localhost:63342";
-
 // capture the "Next" on portfolio page
 $(".next-btn").click(function(){
-    alert("I am clicked")
-    // $("#pic1").attr("href").val("img/portfolio/digital/2.jpg")
-    // $("#pic1").attr("data-setbg").val("img/portfolio/digital/2.jpg")
+    var divtag = pics1;
+    var styletag = "background-image:url(digital-switch/1.jpg)";
+    for (i = 1 ; i <= 23 ; i++) {
+        console.log(i)
+        $("#pics"+i).attr("href", "digital-switch/"+i+".jpg")
+        $("#pics"+i).attr("data-setbg", "digital-switch/"+i+".jpg")
+        $("#pics"+i).attr("style", "background-image:url(digital-switch/"+i+".jpg)")
+    }
 });
