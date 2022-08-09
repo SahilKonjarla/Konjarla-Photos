7331ce7fd67bd6bb7014d5080c4d71d8629983e3
Sahil Konjarla <sahil.konjarla@gmail.com>
2022-08-08 18:14:05 -0700

    start of Konjarla-Photos

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Konjarla-Photos
+This is a repository to have a version control of my website so I can keep track and understand how this works
---
+++ js/pics.js
@@ -0,0 +1,8 @@
+//var searchhost = "localhost:63342";
+
+// capture the "Next" on portfolio page
+$(".next-btn").click(function(){
+    alert("I am clicked")
+    // $("#pic1").attr("href").val("img/portfolio/digital/2.jpg")
+    // $("#pic1").attr("data-setbg").val("img/portfolio/digital/2.jpg")
+});
