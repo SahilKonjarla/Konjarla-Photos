34dab97b103a27e5bb98fc855eed2c921d1dd85f
Sahil Konjarla <sahil.konjarla@gmail.com>
2022-08-17 18:23:08 -0700

    recent changes

--- js/pics.js
+++ js/pics.js
@@ -1,11 +1,10 @@
 // capture the "Next" on portfolio page
 $(".next-btn").click(function(){
-    var divtag = pics1;
-    var styletag = "background-image:url(digital-switch/1.jpg)";
-    for (i = 1 ; i <= 5 ; i++) {
+    for (i = 1; i <= 23; i++) {
         console.log(i)
         $("#pics"+i).attr("href", "digital-switch/"+i+".jpg")
         $("#pics"+i).attr("data-setbg", "digital-switch/"+i+".jpg")
         $("#pics"+i).attr("style", "background-image:url(digital-switch/"+i+".jpg)")
     }
+    $("portfolio-section").refresh();
 });
