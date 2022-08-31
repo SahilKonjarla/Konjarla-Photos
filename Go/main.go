b6207d22b05672c2a9fdd2d4392178d4721ded48
Sahil Konjarla <sahil.konjarla@gmail.com>
2022-08-30 23:15:13 -0700

    starting API

---
+++ Go/main.go
@@ -0,0 +1,13 @@
+package main
+
+// pic represents data about the picture.
+type pic struct {
+	ID    string 'json:"id"'
+	Type  string 'json:"type"'
+	Genre string 'json:"genre"'
+}
+
+// pic slice to seed the pictures
+var pics = []pic {
+	
+}
