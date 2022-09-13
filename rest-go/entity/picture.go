fe069ab63f13d313579f4cca9d4206e0f12ff159
Sahil Konjarla <sahil.konjarla@gmail.com>
2022-09-12 17:29:49 -0700

    added an entity and client to help with db

--- rest-go/entity/picture.go
+++ rest-go/entity/picture.go
@@ -1,1 +1,10 @@
 package entity
+
+// Person object for REST(CRUD)
+type Picture struct {
+	ID       int    `json:"id"`
+	Type     string `json:"type" gorm:"type""`
+	Genre    string `json:"genre" gorm:"genre"`
+	Album    string `json:"album" gorm:"album"`
+	Filename string `json:"filename" gorm:"filename"`
+}
