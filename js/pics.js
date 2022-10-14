e19c68ce57e409ac0412c6a013e1de35a330106c
Sahil Konjarla <97363335+Kotiger1@users.noreply.github.com>
2022-10-14 16:05:56 -0700

    Update pics.js

--- js/pics.js
+++ js/pics.js
@@ -1,5 +1,6 @@
-var rootdef = "localhost:8090";
+var rootDir = "img/";
 var host = "http://localhost:8090"
+
 
 // shows a fail message
 function showfail(msg) {
@@ -45,18 +46,45 @@ function makeRestCall(callback, uri) {
 // processes results taken from the REST Call
 function processResults(response) {
     for (let i = 0; i < 23; i++) {
-        console.log(response[i])
+        var picData = JSON.parse(JSON.stringify(response[i]))
+        var picGenre = picData.genre
+        var picAlbum = picData.album
+        var picFilename = picData.filename
+        $("#pics"+ (i + 1)).attr("href", rootDir + picAlbum + "/JPG/" + picFilename)
+        $("#pics"+ (i + 1)).attr("data-setbg", rootDir + picAlbum + "/JPG/" + picFilename)
+        $("#pics"+ (i + 1)).attr("style", "background-image:url("+'"'+(rootDir + picAlbum + "/JPG/" + picFilename)+'"'+"); height: 298.667px;")
+        $("#genre" + (i + 1)).attr("class", "mix col-xl-2 col-md-3 col-sm-4 col-6 p-0 " + picGenre)
+
+        /*for (;counter < lastCounter; counter++) {
+            picArray[counter] = picGenre
+            picArray[counter] = picURI
+        }
+        if (lastCounter < 46) {
+            lastCounter += 2
+        }*/
     }
 }
 
 
 // capture the "Next" on portfolio page
 $(".next-btn").click(function(){
-    makeRestCall("/pictures/get?type=digital&page=1",processResults)
+    var page = document.querySelector("#button")
+    var currPage = window.location.href
+    console.log(currPage)
+    if (currPage.includes("digital.html") == true) {
+        makeRestCall(processResults, "/pictures/get?type=digital&page="+page.dataset.id)
+        page.dataset.id++
+    } else {
+        makeRestCall(processResults, "/pictures/get?type=film&page="+page.dataset.id)
+        page.dataset.id++
+    }
+    /*for (let i = 0; i < 23; i++) {
+        console.log((result[i]))
+    }
     for (i = 1; i <= 23; i++) {
         // console.log(i)
         $("#pics"+i).attr("href", "digital-switch/"+i+".jpg")
         $("#pics"+i).attr("data-setbg", "digital-switch/"+i+".jpg")
         $("#pics"+i).attr("style", "background-image:url(digital-switch/"+i+".jpg); height: 298.667px;")
-    }
+    }*/
 });
